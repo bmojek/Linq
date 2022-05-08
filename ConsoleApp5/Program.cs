4803ee74539a5658cfad2673ad41d96a0af65f25
Bartłomiej Mojek <Bartlomiej.Mojek@microsoft.wsei.edu.pl>
2022-05-08 12:28:07 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Krzysztof Molenda, 1965-11-20; Jan Kowalski, 1987-01-01; Anna Abacka, 1972-05-20; Józef Kabacki, 2000-01-02; Kazimierz Moksa, 2001-01-02";
+            var query = s.Split(';')
+                        .Select(x => x.Trim())
+                        .Select(x => x.Split(','))
+                        .Select(x => (osoba: x[0].Split(' '), data: x[1]))
+                        .Select(x => (nazwisko: x.osoba[1], imie: x.osoba[0], data : x.data))
+                        .OrderBy(x => x.data)
+                        .ThenBy(x => x.nazwisko)
+                        .ThenBy(x => x.imie);
+
+            foreach(var x in query)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
